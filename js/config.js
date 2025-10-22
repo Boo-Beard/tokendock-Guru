c50fc695c7d721488ba47159dfff9302c69e552d
Boo-Beard <81385452+Boo-Beard@users.noreply.github.com>
2025-10-22 13:15:32 +0400

    Guru Dock

---
+++ js/config.js
@@ -0,0 +1,21 @@
+/* ========= PROJECT CONFIG ========= */
+const PROJECT_CONFIG = {
+  name: "Guru",
+  contractAddress: "0xaA7D24c3E14491aBaC746a98751A4883E9b70843",
+  logoUrl: "https://dd.dexscreener.com/ds-data/tokens/ethereum/0xaa7d24c3e14491abac746a98751a4883e9b70843.png?key=7b4cb5&size=xl",
+  tagline: "Meme meets tech, namaste.",
+  mission: "Guru is a PAMM-inspired, delegated fund management defi protocol that enables anyone to create or join a fund consisting of crypto assets.",
+
+  // URLs
+  tradeUrl: "https://app.uniswap.org/explore/tokens/ethereum/0xaA7D24c3E14491aBaC746a98751A4883E9b70843?inputCurrency=NATIVE",
+  chartUrl: "https://dexscreener.com/ethereum/0x0b11288843e14e43484cd62d7eba1ba20b4ac977",
+  docsUrl: "https://gurufund.gitbook.io/guru/",
+  websiteUrl: "https://guru.fund/",
+  chainExplorerUrl: "https://etherscan.io/token/0xaA7D24c3E14491aBaC746a98751A4883E9b70843",
+
+  // Socials (X, LinkedIn, Discord, Telegram, Medium)
+  socialLinks: [
+    { icon: "fa-brands fa-x-twitter",  url: "https://guru.fund/x" },
+    { icon: "fab fa-telegram",         url: "https://guru.fund/telegram" },
+  ]
+};
